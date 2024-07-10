f321292c7d5d2d4f159378b68c48156efd8a555f
Nurullah649 <113285824+Nurullah649@users.noreply.github.com>
2024-07-10 17:05:45 +0300

    ..
    isimlendirme düzenlemeleri

---
+++ example_sdk/src/main/java/com/example/example_sdk/ProjectInformationSDK.java
@@ -0,0 +1,14 @@
+package com.example.example_sdk;
+
+public class ProjectInformationSDK {
+    private static final String SDK_VERSION = "1.0";
+    private static final String CREATOR_NAME = "Nurullah";
+
+    public String getSdkVersion() {
+        return SDK_VERSION;
+    }
+
+    public String getCreatorName() {
+        return CREATOR_NAME;
+    }
+}
