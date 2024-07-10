10935309aa83ce9e2506956d054fedd145fe02ec
Nurullah649 <113285824+Nurullah649@users.noreply.github.com>
2024-07-10 15:07:32 +0300

    SDK Oluşturma
    SDK için ilk denemeler

---
+++ app/src/main/java/com/example/sdk_deneme/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.sdk_deneme
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.example_sdk.Project_Information_SDK
+import com.example.sdk_deneme.ui.theme.SDK_denemeTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SDK_denemeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val sdkInfo = Project_Information_SDK()
+    val version = sdkInfo.sdkVersion
+
+
+    Tab(selected = true, onClick = { /*TODO*/ }) {
+        
+    }
+    Text(
+        text = "SDK Version: $version",
+        modifier=modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SDK_denemeTheme {
+        Greeting("Android")
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,31 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        google()
+        gradlePluginPortal()
+        flatDir {
+            dirs("libs")
+        }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        flatDir {
+            dirs("libs")
+        }
+    }
+}
+
+rootProject.name = "SDK_deneme"
+include(":app")
+include(":example_sdk")
